a6809370065111fc6e6f5fd6fdf19a72b88612a8
Drige300 <mariusz.kaczmarczyk@microsoft.wsei.edu.pl>
2019-03-17 16:07:01 +0100

    v2

--- Lab01/HelloConsoleApp/Program.cs
+++ Lab01/HelloConsoleApp/Program.cs
@@ -22,6 +22,25 @@ namespace HelloConsoleApp
             Console.WriteLine("Witaj " + imie + " " + nazwisko);
             Console.WriteLine("Witaj {0} {1}. Czy Pan {0} rzeczywiście nazywa się {1}?", imie, nazwisko);
 
+
+            Console.Write("Podaj wiek: ");
+            int wiek = Convert.ToInt32(Console.ReadLine());
+
+            if (wiek < 0)
+            {
+                Console.WriteLine("Błędne dane!!");
+            }
+            else
+            {
+                if (wiek < 67)
+                {
+                    Console.WriteLine("Do emerytury zostało ci {0} lat", 67 - wiek);
+                }
+                else
+                {
+                    Console.WriteLine("Jesteś emerytem!");
+                }
+            }
         }
     }
 }
